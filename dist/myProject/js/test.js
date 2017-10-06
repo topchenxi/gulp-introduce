39d664147e007bde16784bde58ac208fcc0f0c59
c.c <469770982@qq.com>
2017-10-06 14:42:29 +0800

    'version'

--- dist/myProject/js/test.js
+++
@@ -1,1 +0,0 @@
-var array=[2,3,4,5,7,1,9,3],newArray=Array.from(new set(array));console.log(newArray);
--- static/myProject/js/test.js
+++
@@ -1,5 +0,0 @@
-var array = [2, 3, 4, 5, 7, 1, 9, 3];
-
-var newArray = Array.from(new set(array));
-
-console.log(newArray);
